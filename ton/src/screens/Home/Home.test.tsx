5fe2f7964ea8d508fec628385902bccfe5e8925c
AlexandreJr <juniordias_@live.com>
2023-03-24 00:18:41 -0300

    chore: add home

--- ton/src/screens/Home/Home.test.tsx
+++ ton/src/screens/Home/Home.test.tsx
@@ -3,15 +3,55 @@ import React from 'react';
 import { render } from '@testing-library/react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { Home } from './Home';
+
 import { theme } from '../../global';
 import { Provider } from 'react-redux';
 import store from '../../store';
+
+const RESPONSE = {
+  data: [
+    {
+      id: 'T1',
+      title: 'T1 Brother',
+      thumbnail:
+        'https://res.cloudinary.com/dunz5zfpt/fl_progressive/f_auto,c_limit,w_256,q_auto/site-ton/t1-1',
+      price: 118.8,
+    },
+    {
+      id: 'T1_CHIP',
+      title: 'T1 Chip Brother',
+      thumbnail:
+        'https://res.cloudinary.com/dunz5zfpt/fl_progressive/f_auto,c_limit,w_256,q_auto/site-ton/t1-chip-1',
+      price: 190.8,
+    },
+    {
+      id: 'T2',
+      title: 'T2+ Brother',
+      thumbnail:
+        'https://res.cloudinary.com/dunz5zfpt/fl_progressive/f_auto,c_limit,w_256,q_auto/site-ton/t2-1',
+      price: 310.8,
+    },
+    {
+      id: 'T3',
+      title: 'T3 Brother',
+      thumbnail:
+        'https://res.cloudinary.com/dunz5zfpt/fl_progressive/f_auto,c_limit,w_256,q_auto/site-ton/t3-1',
+      price: 418.8,
+    },
+  ],
+};
 
 jest.mock('react-native-vector-icons/MaterialIcons', () => 'Icon');
 
 jest.mock('react-native-iphone-x-helper', () => ({
   getStatusBarHeight: jest.fn(),
   isIphoneX: jest.fn(),
+}));
+
+jest.mock('axios', () => ({
+  create: jest.fn(() => ({
+    get: jest.fn().mockResolvedValue(RESPONSE),
+  })),
 }));
 
 describe('Behavior Home', () => {
@@ -22,6 +62,12 @@ describe('Behavior Home', () => {
       </ThemeProvider>
     </Provider>
   );
+
+  it('render snapshot', () => {
+    const result = render(screenRender()).toJSON();
+
+    expect(result).toMatchSnapshot();
+  });
 
   it('render snapshot', () => {
     const result = render(screenRender()).toJSON();
